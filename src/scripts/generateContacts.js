d7388323ebd9235ed3f1fb49fcffc7057fc67b71
Oleksandra K <a.koroliova@gmail.com>
2024-06-08 19:34:01 +0300

    Add faker-js to dependencies

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,8 @@
 import { PATH_DB } from '../constants/contacts.js';
 
+// опишіть функцію generateContacts. Вона має за допомогою функції createFakeContact,
+//створювати передану кількість згенерованих контактів, а потім додавати їх до масиву у файлі src / db / db.json
+//і записувати їх назад у файл src / db / db.json.
 const generateContacts = async (number) => {};
 
 await generateContacts(5);
