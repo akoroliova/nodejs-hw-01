d4e46883ea71fd2873a07b7f037cf716f5938238
akoroliova <a.koroliova@gmail.com>
2024-06-09 10:09:32 +0300

    Add workdir

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,3 +1,4 @@
 import path from 'node:path';
+import { workdir } from '..';
 
-export const PATH_DB = path.join('src', 'db', 'db.json');
+export const PATH_DB = path.join(workdir, 'src', 'db', 'db.json');
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import path from 'node:path';
+
+export const workdir = path.join(process.cwd());
