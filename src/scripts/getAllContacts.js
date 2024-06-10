30152f90137619523d9d1e0ead0e51073ba54053
Oleksandra K <a.koroliova@gmail.com>
2024-06-10 12:09:19 +0300

    Add getAllContacts logic

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,7 +1,19 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const getAllContacts = async () => {};
+export const getAllContacts = async () => {
+  try {
+    if (!PATH_DB) {
+      throw new Error('Path to the database file is not defined');
+    }
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    if (!data) {
+      throw new Error('File is empty');
+    }
+    return data;
+  } catch (err) {
+    console.error('Read file error:', err);
+  }
+};
 
 console.log(await getAllContacts());
-
-//Функції в файлах скриптів - асинхронні.
