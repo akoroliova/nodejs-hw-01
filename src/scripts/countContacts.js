566368d5b93605729a6a41b75c9f8d92ee343d25
Oleksandra K <a.koroliova@gmail.com>
2024-06-10 12:23:38 +0300

    Add countContacts logic

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,7 +1,21 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const countContacts = async () => {};
+export const countContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    if (!data) {
+      throw new Error('File is empty');
+    }
+    const contacts = JSON.parse(data);
+    if (!Array.isArray(contacts)) {
+      throw new Error('Invalid data format');
+    }
+    const count = contacts.length;
+    return count;
+  } catch (error) {
+    console.error('Помилка читання файлу або парсингу даних:', error);
+  }
+};
 
 console.log(await countContacts());
-
-//Функції в файлах скриптів - асинхронні.
